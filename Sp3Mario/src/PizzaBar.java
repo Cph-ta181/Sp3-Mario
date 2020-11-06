d2f90ab11ccbb52eb63c50a6db36bcf484332c71
Cph-Ta181 <cph-ta181@cphbusiness.dk>
2020-11-06 12:08:11 +0100

    Update PizzaBar.java

--- Sp3Mario/src/PizzaBar.java
+++ Sp3Mario/src/PizzaBar.java
@@ -1,2 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class PizzaBar {
+    private ArrayList<Bestilling> bestillinger = new ArrayList<>();
+    private MenuKort menu;
+
+    public  PizzaBar(MenuKort menu){
+        this.menu = menu;
+    }
+
+    public ArrayList<Bestilling> getBestillinger() {
+        return bestillinger;
+    }
+
+    public MenuKort getMenu() {
+        return menu;
+    }
+
+    private void sortBestillinger(){
+        Collections.sort(bestillinger, new Comparator<Bestilling>() {
+            public int compare(Bestilling b1, Bestilling b2) {
+                return b1.getDatetime().compareTo(b2.getDatetime());
+            }
+        });
+    }
 }
