03dd05ccdf274f7b7c27d2464b6a25789fb6c2e4
firatk00 <70583571+firatk00@users.noreply.github.com>
2020-11-06 12:31:13 +0100

    1234

--- Sp3Mario/src/MenuKort.java
+++ Sp3Mario/src/MenuKort.java
@@ -1,2 +1,23 @@
+import java.io.File;
+import java.util.List;
+
 public class MenuKort {
+    private List<Pizza> pizzas;
+
+    public MenuKort(String filename) {
+        File file = new File("/resources");
+
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void addPizza(Pizza pizzaToAdd){
+        pizzas.add(pizzaToAdd);
+    }
+
+    public void removePizza(Pizza pizzaToRemove) {
+        pizzas.remove(pizzaToRemove);
+    }
 }
