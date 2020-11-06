76ee1d7bbfa2936596fe5d0de052406cc62626b7
firatk00 <70583571+firatk00@users.noreply.github.com>
2020-11-06 12:38:34 +0100

    Update MenuKort.java

--- Sp3Mario/src/MenuKort.java
+++ Sp3Mario/src/MenuKort.java
@@ -1,11 +1,21 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 
 public class MenuKort {
     private List<Pizza> pizzas;
 
     public MenuKort(String filename) {
-        File file = new File("/resources");
+        File file = new File("/resources/pizzaer.txt");
+        try {
+
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
