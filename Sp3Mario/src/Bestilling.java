621325c49373234790a8ca00f70c1b90deef1e73
Mathias Precht <math735a@gmail.com>
2020-11-06 11:33:38 +0100

    Update Bestilling.java

--- Sp3Mario/src/Bestilling.java
+++ Sp3Mario/src/Bestilling.java
@@ -1,2 +1,42 @@
+
+import java.util.List;
+import java.util.GregorianCalendar;
+
+
 public class Bestilling {
+    private  List<Pizza> pizzas;
+    private  GregorianCalendar datetime;
+    private  Boolean paid;
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public GregorianCalendar getDatetime() {
+        return datetime;
+    }
+
+    public Boolean getPaid() {
+        return paid;
+    }
+
+    public int totalPrice(){
+       int totalPrice = 0;
+       for (Pizza pizza:pizzas) {
+            //totalPrice+=pizza.getPrice;
+
+       }
+
+        return totalPrice;
+    }
+
+
+   public void addPizza(Pizza pizzaToAdd){
+       pizzas.add(pizzaToAdd);
+   }
+
+   public void removePizza(Pizza pizzaToRemove){
+
+        pizzas.remove(pizzaToRemove);
+   }
 }
