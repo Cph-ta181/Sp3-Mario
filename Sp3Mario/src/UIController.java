809abd9898bd721adad6e06a21e77a02a325742b
DanielD99 <71074562+DanielD99@users.noreply.github.com>
2020-11-06 11:58:40 +0100

    UIController

--- Sp3Mario/src/UIController.java
+++ Sp3Mario/src/UIController.java
@@ -1,2 +1,74 @@
-public class UIController {
-}
+//@Daniel
+
+import java.util.List;
+import java.util.Scanner;
+
+    public class UIController {
+        Scanner sc = new Scanner(System.in);
+
+        public void runApp(){
+            int run = 0;
+            printMenuKort();
+            while (run != 9) {
+
+                run = sc.nextInt();
+                switch (run) {
+                    case 1:visPizza();break;
+                    case 2:bestilPizza();break;
+                    case 3:visAktiveBestillinger();break;
+                    case 9:exit();break;
+                    default: exit();
+                }
+
+
+            }
+
+
+
+        }
+
+        private void exit() {
+        }
+
+        private void visAktiveBestillinger() {
+        }
+
+        private void bestilPizza() {
+        }
+
+        private void visPizza() {
+        }
+
+        private void printMenuKort() {
+        }
+
+
+        @Override
+        public List<Pizza> getPizza() {
+            return null;
+        }
+
+        @Override
+        public List<Bestilling> getAlleOrdrer() {
+            return null;
+        }
+
+        @Override
+        public Bestilling getOrderById(int id) {
+            return null;
+        }
+
+        @Override
+        public void saveOrder(Bestilling ordrer) {
+
+        }
+
+        @Override
+        public void markOrderAsDone(Bestilling ordrer) {
+
+        }
+        @Override
+        public void markOrderAsDone(Bestilling ordrer) {
+
+        }
+    }
