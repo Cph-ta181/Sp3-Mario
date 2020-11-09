764f3cfeb2a44b4f2ea023416af8512f2f99c34c
Cph-Ta181 <cph-ta181@cphbusiness.dk>
2020-11-09 12:25:58 +0100

    Added saving and reading completed orders

--- Sp3Mario/src/PizzaBar.java
+++ Sp3Mario/src/PizzaBar.java
@@ -1,18 +1,19 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+
+import java.io.*;
+import java.util.*;
 
 public class PizzaBar {
     private ArrayList<Bestilling> bestillinger = new ArrayList<>();
+    private ArrayList<Bestilling> completedBestillinger = new ArrayList<>();
     private MenuKort menu;
 
     public  PizzaBar(MenuKort menu){
         this.menu = menu;
+        this.completedBestillinger = readCompletedOrders();
     }
 
     public ArrayList<Bestilling> getBestillinger() {
-        sortBestillinger();
+        sortBestillinger(bestillinger);
         return bestillinger;
     }
 
@@ -20,23 +21,51 @@ public class PizzaBar {
         return menu;
     }
 
-    private void sortBestillinger(){
-        Collections.sort(bestillinger, new Comparator<Bestilling>() {
+    private void sortBestillinger(ArrayList<Bestilling> arrList){
+        Collections.sort(arrList, new Comparator<Bestilling>() {
             public int compare(Bestilling b1, Bestilling b2) {
                 return b1.getDatetime().compareTo(b2.getDatetime());
             }
         });
     }
 
-    public int calculateLastMonthEarnings(){
-        int earnings = 0;
-
-        return earnings;
+    public void removeBestilling(Bestilling bestilling){
+        bestillinger.remove(bestilling);
+        completedBestillinger.add(bestilling);
+        saveCompletedOrders();
     }
 
-    public int calcultateTodaysEarnings(){
-        int earnings = 0;
+    private void saveCompletedOrders(){
+        try {
+            FileOutputStream fop = new FileOutputStream("/resources/completedOrders.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fop);
+            oos.writeObject(completedBestillinger);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ArrayList<Bestilling> readCompletedOrders(){
+        ArrayList<Bestilling> orders = new ArrayList<>();
+        try {
+            FileInputStream fis = new FileInputStream("/resources/completedOrders.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            orders = (ArrayList<Bestilling>)ois.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return orders;
+    }
+
+    public int calculateThisMonthEarnings(){
+        int earnings = 0;
+        sortBestillinger(completedBestillinger);
+
+        for (int i = 0; completedBestillinger.get(i).getDatetime().get(Calendar.MONTH) == Calendar.MONTH; i++){
+            earnings += completedBestillinger.get(i).totalPrice();
+        }
         return earnings;
     }
 
